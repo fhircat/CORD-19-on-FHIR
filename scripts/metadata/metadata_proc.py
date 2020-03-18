4ba386f7b5e552bd78041984d85ac5a803b60a5b
hsolbrig <solbrig@jhu.edu>
2020-03-18 13:26:25 -0500

    Add id field

--- scripts/metadata/metadata_proc.py
+++ scripts/metadata/metadata_proc.py
@@ -10,7 +10,9 @@ with open('../../datasets/all_sources_metadata_2020-03-13.csv') as f:
     for row in reader:
         row_j = JsonObj(**row)
         if not printed:
-            print(as_json(JsonObj(**row)))
+            row_j = JsonObj(**row)
+            row_j.id = row_j.source_x + '/' + row_j.sha
+            print(as_json(row_j))
             printed = True
         known_sources.setdefault(row_j.source_x, 0)
         known_sources[row_j.source_x] += 1
