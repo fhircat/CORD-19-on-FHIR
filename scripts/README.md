8c798ebe95de4e36df63ba0cc3db04576af3228b
hsolbrig <solbrig@jhu.edu>
2020-03-19 11:03:29 -0500

    Checkpoint - one more coming

--- scripts/README.md
+++ scripts/README.md
@@ -1,4 +1,4 @@
 # Scripts directory
 
 ## gen_datasets.py
-Run this from the scripts/metadata directory.  This will take all zip files in the root
+```bas```
--- scripts/metadata/__init__.py
+++ scripts/metadata/__init__.py
@@ -1,1 +1,7 @@
-DATASETS = '../../datasets'
+import os
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+
+DATASETS_DIR = os.path.join(ROOT_DIR, 'datasets')
+METADATA_DIR = os.path.join(DATASETS_DIR, 'metadata')
+CONTEXT_DIR = os.path.join(ROOT_DIR, 'contexts')
