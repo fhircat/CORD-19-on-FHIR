7d7a2d29bc0ab7580da71a2fcdaae02c04235e69
hsolbrig <solbrig@jhu.edu>
2020-03-26 14:17:52 -0500

    Zip ttl and json in separate files, use better compression algorithm

--- scripts/metadata/metadata_zipper.py
+++ scripts/metadata/metadata_zipper.py
@@ -1,36 +1,60 @@
 import os
 from typing import Optional
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED
 
 from scripts.metadata import DATASETS_DIR, METADATA_DIR
 
-print(f"** Zipping metadata directory **")
 
-files_per_zipfile = 20000
-zipfile_series = 0
-nfiles = 0
-zf: ZipFile = None
+class Zipper:
+    FILES_PER_ZIPFILE = 60000
+
+    def __init__(self, base_dir: str, suffix: str) -> None:
+        self.base_dir = base_dir
+        self.suffix = suffix
+        self.zipfile_series = 0
+        self.nfiles_in_zipfile = 0
+        self.total_files = 0
+        self.zipfile: Optional[ZipFile] = None
+
+    def addfile(self, fname: str) -> None:
+        if self.zipfile is None or self.nfiles_in_zipfile >= Zipper.FILES_PER_ZIPFILE:
+            self.rollover_zipfile()
+        self.zipfile.write(fname, os.path.basename(fname))
+        self.nfiles_in_zipfile += 1
+        self.total_files += 1
+
+    def rollover_zipfile(self) -> None:
+        self.close()
+        self.zipfile_series += 1
+        zf_name = f"metadata_{self.suffix}_{self.zipfile_series}.zip"
+        print(f"Writing {zf_name}")
+        self.zipfile = ZipFile(os.path.join(self.base_dir, zf_name), "w", ZIP_DEFLATED)
+        self.nfiles_in_zipfile = 0
+
+    def close(self) -> None:
+        if self.zipfile is not None:
+            self.zipfile.close()
+
+    def summary(self) -> str:
+        return f"{self.total_files} {self.suffix} files written"
 
 
-def new_zipfile(current_zipfile: Optional[ZipFile]) -> ZipFile:
-    global zipfile_series
+json_zipper = Zipper(DATASETS_DIR, "json")
+ttl_zipper = Zipper(DATASETS_DIR, "ttl")
 
-    if current_zipfile:
-        current_zipfile.close()
-    zipfile_series += 1
-    zf_name = f"metadata{zipfile_series}.zip"
-    print(f"\tWriting {zf_name}")
-    zf = ZipFile(os.path.join(DATASETS_DIR, zf_name), "w")
-    return zf
-
-
+print(f"** Zipping {METADATA_DIR} **")
 for file in os.listdir(METADATA_DIR):
     _, suffix = os.path.splitext(file)
-    if zf is None or nfiles > files_per_zipfile:
-        zf = new_zipfile(zf)
-        nfiles = 0
-    zf.write(os.path.join(METADATA_DIR, file), os.path.join(suffix[1:], file))
-    nfiles += 1
+    fname = os.path.join(METADATA_DIR, file)
+    if suffix == '.json':
+        json_zipper.addfile(fname)
+    elif suffix == ".ttl":
+        ttl_zipper.addfile(fname)
+    else:
+        print(f"Skipping {file}")
 
-zf.close()
-print(f"** {nfiles} written **")
+json_zipper.close()
+print(json_zipper.summary())
+ttl_zipper.close()
+print(ttl_zipper.summary())
+
